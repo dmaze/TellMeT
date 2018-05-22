cf5179a417c8e2cc52ff733de0c2a61677af5c1c
David Maze <dzmaze@gmail.com>
2018-05-21 20:43:35 -0400

    Burn the last remnants of the counter example

--- shared/TellMeT/Util.hs
+++ shared/TellMeT/Util.hs
@@ -1,7 +1,2 @@
+-- | Miscellaneous utilities.
 module TellMeT.Util where
-
-import Data.Default (Default(def))
-import Lens.Micro (ASetter, set)
-
-make :: (Default a) => ASetter a a x b -> b -> a
-make setter param = set setter param def
