9d1ca0d1227cfe389f54098f85de20a1d6f26ee2
David Maze <dzmaze@gmail.com>
2018-06-26 19:31:17 -0400

    RoutePage: do a topological sort of the stops across the visible trips

--- shared/TellMeT/Components/RoutePage.hs
+++ shared/TellMeT/Components/RoutePage.hs
@@ -12,6 +12,7 @@ import           Data.List                          (find)
 import           Data.Map                           (Map)
 import qualified Data.Map                           as Map
 import           Data.Monoid                        ((<>))
+import           Data.Set                           (Set)
 import qualified Data.Set                           as Set
 import           Lens.Micro                         (at, ix, non, (^.), (^..))
 import           Miso.Html                          (View, text)
@@ -163,12 +164,60 @@ viewStopRow theStopId sts =
        [] $
        [_th_] <> (_td_ <$> sts) ]
 
+-- | Come up with a sensible ordering for the set of stops in trips.
+-- If the ordering of stops across all trips makes a DAG (e.g., all
+-- trips visit stops in the same order, but some might skip some) then
+-- this should be a topological sort; but we don't actually have that
+-- guarantee.
 orderStops :: [[StopTime]] -> [Identifier Stop]
-orderStops sts = foldl' loop1 [] sts
-  where loop1 found stList = foldl' loop2 found stList
-        loop2 found st = if stopTimeStopId st `elem` found
-                         then found
-                         else found <> [stopTimeStopId st]
+orderStops sts = orderStopIds $ fmap stopTimeStopId <$> sts
+
+-- | Come up with a sensible ordering for the set of sorted stop IDs
+-- in trips.  Implementation of 'orderStops', where the stop IDs
+-- are known to be sorted.
+orderStopIds :: (Ord a) => [[a]] -> [a]
+orderStopIds stopIds = orderStopGraph stopGraph []
+  -- We're going to build up a map from node ID (stop ID) to a set of
+  -- its predecessor stop IDs.  That means, for each path (trip), we
+  -- need to create the entries id0 -> (), id1 -> (id0), id2 -> (id1),
+  -- and so on.  Then we need to union the resulting maps across all
+  -- of the trips.
+  where stopGraph = foldl' (Map.unionWith Set.union) Map.empty $
+                    tripToPreds <$> stopIds
+        tripToPreds []     = Map.empty
+        tripToPreds (x:xs) = tripToPreds' x xs (Map.singleton x Set.empty)
+        tripToPreds' _ [] m = m
+        tripToPreds' p (x:xs) m =
+          tripToPreds' x xs $
+          Map.insertWith Set.union x (Set.singleton p) m
+
+-- | Do a mostly topological sort of the stop graph.
+orderStopGraph :: (Ord a) => Map a (Set a) -> [a] -> [a]
+orderStopGraph preds [] =
+  let (roots', more) = Map.partition Set.null preds
+      roots = Map.keys roots'
+  in if null roots
+     then if Map.null more
+          then
+            -- both the graph and the worklist are empty; all done
+            []
+          else
+            -- there are no roots, and the worklist is empty,
+            -- which means every node in the graph has predecessors;
+            -- that should imply there is a cycle in the graph, so
+            -- pick something arbitrarily and move on
+            let ((anything, _), more') = Map.deleteFindMin more
+            in orderStopGraph more' [anything]
+     else
+       -- there is at least one root, use it/them as the new
+       -- worklist
+       orderStopGraph more roots
+orderStopGraph preds (x:xs) =
+  -- There is at least one item in the worklist.  Do the first
+  -- thing in the worklist by removing it from all of the predecessor
+  -- sets and emitting it as a result.
+  let preds' = (Set.delete x) <$> preds
+  in (x:orderStopGraph preds' xs)
 
 viewOptionalFeature :: MisoString -> Maybe Bool -> [View action]
 viewOptionalFeature _ Nothing = []
